d8f757a363a6ed7128de8771d3032a6d7d11fc40
AndrewAV <andrew.jose.av@gmail.com>
2016-10-17 00:38:33 -0600

    Cambios de las matrices e imagenes

--- Proyecto III - v.0/Proyecto III - v.0/Juego.Designer.cs
+++ Proyecto III - v.0/Proyecto III - v.0/Juego.Designer.cs
@@ -36,6 +36,7 @@
             this.tableLayoutPanel_EnemigoFacil = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel_EnemigoMedio = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel_EnemigoDificil = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -232,11 +233,22 @@
             this.tableLayoutPanel_EnemigoDificil.Size = new System.Drawing.Size(320, 320);
             this.tableLayoutPanel_EnemigoDificil.TabIndex = 7;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(403, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Listo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_Listo);
+            // 
             // Juego
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(889, 445);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.tableLayoutPanel_EnemigoDificil);
             this.Controls.Add(this.tableLayoutPanel_EnemigoMedio);
             this.Controls.Add(this.tableLayoutPanel_EnemigoFacil);
@@ -263,5 +275,6 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_EnemigoFacil;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_EnemigoMedio;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_EnemigoDificil;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Proyecto III - v.0/Proyecto III - v.0/Juego.cs
+++ Proyecto III - v.0/Proyecto III - v.0/Juego.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Proyecto_III___v._0
 {
     public partial class Juego : Form
@@ -18,6 +19,8 @@ namespace Proyecto_III___v._0
             cargar(dificultad);
         }
 
+
+        //Metodo que carga las matrices con el tamano deseado.
         public void cargar(int tamano)
         {
             if (tamano == 5)
@@ -30,15 +33,41 @@ namespace Proyecto_III___v._0
                     {
 
                         Button a = new Button();
+                        Button b = new Button();
+
                         a.Height = 80;
                         a.Width = 80;
                         a.Margin = new System.Windows.Forms.Padding(0);
-                        a.Text = "dfsadf";
+                        a.Text = "" + i + "," + j + "";
+                        a.Tag = "" + i + "," + j + "";
+                        a.Name = "0";
+                       
+
+                        b.Height = 80;
+                        b.Width = 80;
+                        b.Margin = new System.Windows.Forms.Padding(0);
+                        b.Text = "" + i + "," + j + "";
+                        b.Tag = "" + i + "," + j + "";
+
+                        if (a.Name == "0")
+                            a.BackgroundImage = Image.FromFile(@"Agua.png");
+                        else
+                            a.BackgroundImage = Image.FromFile(@"Nada.png");
+
+                        a.Click += (s, e) =>
+                        {
+                            buttonAction(a,tamano);
+                        };
+
+                       
                         tableLayoutPanel_AliadoFacil.Controls.Add(a);
+                        tableLayoutPanel_EnemigoFacil.Controls.Add(b);
                     }
                 }
                 tableLayoutPanel_AliadoDificil.Visible = false;
                 tableLayoutPanel_AliadoMedio.Visible = false;
+                tableLayoutPanel_EnemigoDificil.Visible = false;
+                tableLayoutPanel_EnemigoMedio.Visible = false;
             }
             else if (tamano == 10)
             {
@@ -50,15 +79,28 @@ namespace Proyecto_III___v._0
                     {
 
                         Button a = new Button();
+                        Button b = new Button();
+
                         a.Height = 80;
                         a.Width = 80;
                         a.Margin = new System.Windows.Forms.Padding(0);
-                        a.Text = "dfsadf";
+                        a.Text = "" + i + "," + j + "";
+                        a.Tag = "" + i + "," + j + "";
+                       
+                        b.Height = 80;
+                        b.Width = 80;
+                        b.Margin = new System.Windows.Forms.Padding(0);
+                        b.Text = "" + i + "," + j + "";
+                        b.Tag = "" + i + "," + j + "";
+
                         tableLayoutPanel_AliadoMedio.Controls.Add(a);
+                        tableLayoutPanel_EnemigoMedio.Controls.Add(b);
                     }
                 }
                 tableLayoutPanel_AliadoFacil.Visible = false;
                 tableLayoutPanel_AliadoDificil.Visible = false;
+                tableLayoutPanel_EnemigoFacil.Visible = false;
+                tableLayoutPanel_EnemigoDificil.Visible = false;
             }
             else 
             {
@@ -70,17 +112,49 @@ namespace Proyecto_III___v._0
                     {
 
                         Button a = new Button();
+                        Button b = new Button();
+
                         a.Height = 80;
                         a.Width = 80;
                         a.Margin = new System.Windows.Forms.Padding(0);
-                        a.Text = "dfsadf";
-                        tableLayoutPanel_AliadoDificil.Controls.Add(a);
+                        a.Text = "" + i + "," + j + "";
+                        a.Tag = "" + i + "," + j + "";
+                        a.BackgroundImage = Image.FromFile(@"Agua.png");
+
+                        b.Height = 80;
+                        b.Width = 80;
+                        b.Margin = new System.Windows.Forms.Padding(0);
+                        b.Text = "" + i + "," + j + "";
+                        b.Tag = "" + i + "," + j + "";
+
+                       tableLayoutPanel_AliadoDificil.Controls.Add(a);
+                       tableLayoutPanel_EnemigoDificil.Controls.Add(b);
                     }
                 }
                 tableLayoutPanel_AliadoFacil.Visible = false;
                 tableLayoutPanel_AliadoMedio.Visible = false;
+                tableLayoutPanel_EnemigoFacil.Visible = false;
+                tableLayoutPanel_EnemigoMedio.Visible = false;
             }
         }
+
+
+        //Metodo para que escojer donde colocar el barco.
+        private void buttonAction(Button boton,int tamano) {
+            if (boton.Name == "0")
+            {
+                boton.Name = "1";
+                boton.BackgroundImage = Image.FromFile(@"Nada.png");
+            }
+            else if (boton.Name == "1")
+            {
+                boton.Name = "0";
+                boton.BackgroundImage = Image.FromFile(@"Agua.png");
+            }
+            
+            this.Refresh();
+        }
+
 
         private void Juego_Load(object sender, EventArgs e)
         {
@@ -105,6 +179,14 @@ namespace Proyecto_III___v._0
         private void tableLayoutPanel_AlidadoMedio_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void button_Listo(object sender, EventArgs e)
+        {
+            tableLayoutPanel_AliadoFacil.Enabled = false;
+            tableLayoutPanel_AliadoMedio.Enabled = false;
+            tableLayoutPanel_AliadoDificil.Enabled = false;
+            button1.Visible = false;
         }
     }
 }
